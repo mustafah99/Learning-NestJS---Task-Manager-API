8d1244a58fbe48c028aa518cf91e5ae01c8795e1
Mustafa Said Helal <mustafasaidhelal@gmail.com>
2022-01-09 21:57:31 +0100

    First Commit

---
+++ src/tasks/tasks.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+
+// First to create a new module, use 'nest g mo [nameOfModule]' with Nest CLI to generate module
+@Module({
+  // Second step to generate a controller is to use 'nest g co [nameOfController]' to generate a Controller.
+  // NOTE: Important to use the same name for the module, controller and service when generating all three files.
+  controllers: [TasksController],
+  // Third step is to generate a service to be used by the controller.
+  // To do this, use 'nest g s [nameOfService]' in the Nest cli to generate the provided files for the controller.
+  providers: [TasksService],
+})
+export class TasksModule {}
